a6bc020c40649b7c1c9ccb104745b140cad3598f
Enrique Olvera <enrique.olveragarcia4@t-mobile.com>
2017-03-24 14:25:30 -0700

    second commit

--- app/app.component.js
+++ app/app.component.js
@@ -16,16 +16,31 @@ var AppComponent = (function () {
         //    content: "Learn to Code",
         //    completed: false        
         // }
-        this.tasksme = [
-            new task_1.Task("Buy a monkey", false),
-            new task_1.Task("Walk the turtle", false)
-        ];
+        this.tasksme = [];
+        //   private tasksme = [      //--- removed at the end to polish the "app" ---
+        //        new Task(         
+        //            "Buy a monkey",
+        //            false
+        //        ),
+        //        new Task(
+        //            "Walk the turtle",
+        //            false
+        //        )
+        //   ]
+        this.currentTask = new task_1.Task(null, false); //new component property. Initialization of the object
     }
+    // creating a new function or method
+    AppComponent.prototype.addTask = function () {
+        var task = new task_1.Task(this.currentTask.content, this.currentTask.completed); // creating a new task values
+        this.tasksme.push(task); // adding to the task array
+        this.currentTask.content = null;
+    };
     AppComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'my-app',
-            templateUrl: 'app.component.html' //external template
+            templateUrl: 'app.component.html',
+            styleUrls: ['app.component.css']
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -5,21 +5,32 @@ import { Task } from './model/task';
 @Component({
     moduleId: module.id,
     selector: 'my-app',
-    templateUrl: 'app.component.html' //external template
+    templateUrl: 'app.component.html', //external template
+    styleUrls: [ 'app.component.css' ]
 })
 export class AppComponent {
     //task1: Task = {
     //    content: "Learn to Code",
     //    completed: false        
    // }
-   private tasksme = [
-        new Task(
-            "Buy a monkey",
-            false
-        ),
-        new Task(
-            "Walk the turtle",
-            false
-        )
-   ]
+
+     private tasksme: Task[] = [];
+//   private tasksme = [      //--- removed at the end to polish the "app" ---
+//        new Task(         
+//            "Buy a monkey",
+//            false
+//        ),
+//        new Task(
+//            "Walk the turtle",
+//            false
+//        )
+//   ]
+   private currentTask = new Task(null, false);   //new component property. Initialization of the object
+
+   // creating a new function or method
+   addTask()  {
+       let task = new Task(this.currentTask.content, this.currentTask.completed);  // creating a new task values
+       this.tasksme.push(task);         // adding to the task array
+       this.currentTask.content = null;
+   } 
  }
